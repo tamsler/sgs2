9bc830cac50e52e11408e0e98b12c3c896a6c490
Thomas Amsler <tamsler@gmail.com>
2009-05-11 00:11:40 -0700

    Started to work on the UI. This is pretty much in place, except that the tool height is not quite working when running in Sakai.

--- client/src/java/org/sakaiproject/sgs2/client/Sgs2.java
+++ client/src/java/org/sakaiproject/sgs2/client/Sgs2.java
@@ -1,144 +1,104 @@
+/**********************************************************************************
+ *
+ * Copyright (c) 2009 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
 package org.sakaiproject.sgs2.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.rpc.ServiceDefTarget;
 import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.VerticalPanel;
+
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class Sgs2 implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
 
-	/**
-	 * Create a remote service proxy to talk to the server-side Groovy Shell Service.
-	 */
+	// GWT RPC proxy
 	private GroovyShellServiceAsync groovyShellService = GWT.create(GroovyShellService.class);
 
-	/**
-	 * This is the entry point method.
-	 */
+
 	public void onModuleLoad() {
-		
+
+		// This is a reference for the declared servlet in Sgs2.gwt.xml
 		((ServiceDefTarget) groovyShellService).setServiceEntryPoint(GWT.getModuleBaseURL() + "rpc/sgs2");
+
+		// Layout
+		VerticalPanel verticalPanel = new VerticalPanel();
+		verticalPanel.setWidth("700px");
+		
+		// Widgets
+		final TextArea textArea = new TextArea();
+		final Button submitButton = new Button("Submit");
+		
+		// Handlers
+		ClickHandler clickHandler = new ClickHandler() {
+
+			public void onClick(ClickEvent event) {
+
+				submitButton.setEnabled(false);
+				String sourceCode = textArea.getText();
+
+				groovyShellService.submit(sourceCode, new AsyncCallback<String>() {
+
+					public void onFailure(Throwable caught) {
+						Window.alert("onFailure: groovyShellService.submit(...)");
+						submitButton.setEnabled(true);
+					}
+
+					public void onSuccess(String result) {
+						Window.alert("onSuccess: result = " + result);
+						submitButton.setEnabled(true);
+					}
+				});
+			}
+		};
+		
+		submitButton.addClickHandler(clickHandler);
 				
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
+		// CSS
+		submitButton.addStyleName("submitButton");
 
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
+		// Text Area Configuration
+		textArea.setWidth("100%");
+		textArea.setVisibleLines(25);
+		
+		// Adding widgets to vertical panel
+		verticalPanel.add(textArea);
+		verticalPanel.add(submitButton);
+		
+		// Configure vertical panel
+		verticalPanel.setCellHorizontalAlignment(submitButton, HasHorizontalAlignment.ALIGN_RIGHT);
+		verticalPanel.setSpacing(5);
 
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				sendButton.setEnabled(false);
-				String textToServer = nameField.getText();
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				groovyShellService.submitSourceCode(textToServer,
-						new AsyncCallback<String>() {
-							public void onFailure(Throwable caught) {
-								// Show the RPC error message to the user
-								dialogBox
-										.setText("Remote Procedure Call - Failure");
-								serverResponseLabel
-										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-
-							public void onSuccess(String result) {
-								dialogBox.setText("Remote Procedure Call");
-								serverResponseLabel
-										.removeStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(result);
-								dialogBox.center();
-								closeButton.setFocus(true);
-							}
-						});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		// Adding widget(s) to root panel
+		RootPanel.get("verticalPanel").add(verticalPanel);
+		
+		// Focus the cursor on the text area when the application loads
+		textArea.setFocus(true);
+		textArea.selectAll();
 	}
 }
--- client/src/java/org/sakaiproject/sgs2/server/mock/GroovyShellServiceMockImpl.java
+++ client/src/java/org/sakaiproject/sgs2/server/mock/GroovyShellServiceMockImpl.java
@@ -1,3 +1,21 @@
+/**********************************************************************************
+ *
+ * Copyright (c) 2009 The Sakai Foundation.
+ * 
+ * Licensed under the Educational Community License, Version 1.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.opensource.org/licenses/ecl1.php
+ * 
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ *
+ **********************************************************************************/
+
 package org.sakaiproject.sgs2.server.mock;
 
 import org.sakaiproject.sgs2.client.GroovyShellService;
@@ -8,9 +26,9 @@ public class GroovyShellServiceMockImpl extends RemoteServiceServlet implements 
 
 	private static final long serialVersionUID = 1L;
 
-	public String submitSourceCode(String sourceCode) {
+	public String submit(String sourceCode) {
 
-		return "Groovy Shell Service ...";
+		return "Groovy Shell Service received source code = " + sourceCode;
 	}
 
 }
